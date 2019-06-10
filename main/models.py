891fcf1ff31c08b042debd3ab1f0196038b0f85c
Mateo Alonso Pabón García <46111186+mate458@users.noreply.github.com>
2019-06-10 11:15:22 -0500

    Add files via upload

--- main/models.py
+++ main/models.py
@@ -3,14 +3,14 @@ from django.db import models
 # Create your models here.
 
 class Experiencia(models.Model):
-    IdExperiencia = models.IntegerField(verbose_name = "EXPERIENCIA")
-    IdUsuarios = models.IntegerField(verbose_name = "USUARIOS")
-    IdTipoUsua = models.IntegerField (verbose_name = "TIPOUSUARIO")
-    IdCargos = models.IntegerField (verbose_name = "CARGOS")
-    IdTipoCarg = models.IntegerField (verbose_name = "TIPOCARGO")
-    IdCiudades = models.IntegerField(verbose_name = "CIUDADES")
-    IdDepartamentos = models.IntegerField (verbose_name = "DEPARTAMENTOS")
-    IdPaises = models.IntegerField (verbose_name = "PAISES")
+    IdExperiencia = models.IntegerField(verbose_name = "Experiencia")
+    IdUsuarios = models.IntegerField(verbose_name = "Usuario")
+    IdTipoUsua = models.ForeignKey (TipoUsua, on_delete = models.CASCADE, verbose_name = "Tipo Usuario")
+    IdCargos = models.ForeignKey (Cargos, on_delete = models.CASCADE ,verbose_name = "Cargo")
+    IdTipoCarg = models.ForeignKey (TipoCarg, on_delete = models.CASCADE ,verbose_name = "Tipo Cargo")
+    IdCiudades = models.ForeignKey(Ciudades, on_delete = models.CASCADE ,verbose_name = "Ciudad")
+    IdDepartamentos = models.ForeignKey (Departamentos, on_delete = models.CASCADE ,verbose_name = "Departamento")
+    IdPaises = models.ForeignKey (Paises, on_delete = models.CASCADE ,verbose_name = "Pais")
     Empresas = models.CharField(max_length = 250, default = "", verbose_name = "EMPRESA")
     FechaIni = models.DateTimeField(auto_now=True, verbose_name = "FECHAINICIO")
     FechaFin = models.DateTimeField( auto_noe=True, verbose_name = "FECHAFINAL")
