f78b1b94d48c39872133a4feecc5ba5b9fcb3408
Mateo Alonso Pabón García <46111186+mate458@users.noreply.github.com>
2019-06-10 09:49:39 -0500

    Add files via upload

---
+++ main/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Experiencia(models.Model):
+    IdExperiencia = models.IntegerField(verbose_name = "EXPERIENCIA")
+    IdUsuarios = models.IntegerField(verbose_name = "USUARIOS")
+    IdTipoUsua = models.IntegerField (verbose_name = "TIPOUSUARIO")
+    IdCargos = models.IntegerField (verbose_name = "CARGOS")
+    IdTipoCarg = models.IntegerField (verbose_name = "TIPOCARGO")
+    IdCiudades = models.IntegerField(verbose_name = "CIUDADES")
+    IdDepartamentos = models.IntegerField (verbose_name = "DEPARTAMENTOS")
+    IdPaises = models.IntegerField (verbose_name = "PAISES")
+    Empresas = models.CharField(max_length = 250, default = "", verbose_name = "EMPRESA")
+    FechaIni = models.DateTimeField(auto_now=True, verbose_name = "FECHAINICIO")
+    FechaFin = models.DateTimeField( auto_noe=True, verbose_name = "FECHAFINAL")
+    Funciones = models.CharField(max_length = 500, default = "", verbose_name = "FUNCION")
+    Logros = models.CharField(max_length = 500, default = "", verbose_name = "LOGRO")
