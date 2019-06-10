51d435bf6f49cd81254cfd7e8404235996305e0c
Mateo Alonso Pabón García <46111186+mate458@users.noreply.github.com>
2019-06-10 10:55:59 -0500

    Add files via upload

--- parametros/admin.py
+++ parametros/admin.py
@@ -5,6 +5,8 @@ from .models import TipoDocu
 from .models import EstaCivil
 from .models import TipoEstu
 from .models import TipoLogr
+from .models import Cargos
+from .models import Logros
 
 # Register your models here.
 admin.site.register(Etnia)
@@ -12,3 +14,16 @@ admin.site.register(TipoDocu)
 admin.site.register(EstaCivil)
 admin.site.register(TipoEstu)
 admin.site.register(TipoLogr)
+class CargoModelAdmin(admin.ModelAdmin):
+    list_display = ('NOMBRE', 'ES CARGO')
+    list_display_links = ('NOMBRE')
+    search_fields = ('NOMBRE')
+    list_filter = ('ES CARGO')
+admin.site.register(Cargos)
+
+class LogrosModelAdmin(admin.ModelAdmin):
+    list_display = ('Nombre', 'Descripcion')
+    list_display_links = ('Nombre')
+    search_fields = ('Nombre')
+    list_filter = ('Nombre')
+admin.site.register(Logros)
--- parametros/models.py
+++ parametros/models.py
@@ -1,32 +1,74 @@
 from django.db import models
-
 # Create your models here.
 class Etnia(models.Model):
     NombEtni = models.CharField(max_length = 50)
-
-    def __unicode__(self):
+    class Meta():
+        verbose_name="Etnia"
+        verbose_name_plural = "Etnias"
+    
+    def __str__(self):
         return self.NombEtni
 
 class TipoDocu(models.Model):
     NombTiDo = models.CharField(max_length = 50)
-
-    def __unicode__(self):
+    class Meta():
+        verbose_name="TipoDocumento"
+        verbose_name_plural = "TipoDocumentos"
+    
+    def __str__(self):
         return self.NombTiDo
 
 class EstaCivil(models.Model):
     NombEsCi = models.CharField(max_length = 50)
-
-    def __unicode__(self):
+    class Meta():
+        verbose_name="EstadoCivil"
+        verbose_name_plural = "EstadosCiviles"
+    
+    def __str__(self):
         return self.NombEsCi
 
 class TipoEstu(models.Model):
     NombTiEs = models.CharField(max_length = 50)
-
-    def __unicode__(self):
+    class Meta():
+        verbose_name="TipoEstudio"
+        verbose_name_plural = "TipoEstudios"
+    
+    def __str__(self):
         return self.NombTiEs
 
 class TipoLogr(models.Model):
     NombTiLo = models.CharField(max_length = 50)
 
-    def __unicode__(self):
+    class Meta():
+        verbose_name="TipoLogro"
+        verbose_name_plural = "TipoLogros"
+    def __str__(self):
         return self.NombTiLo
+    
+class Cargos(models.Model):
+    DomiCargo = models.CharField(max_length = 1, default = "", verbose_name = "DOMINIO")
+    ClasCargo = models.CharField(max_length = 2, default = "", verbose_name = "CLASE")
+    OrdeCargo = models.CharField(max_length = 3, default = "", verbose_name = "ORDEN")
+    GeneCargo = models.CharField(max_length = 4, default = "", verbose_name = "GENERO")
+    NomCargo = models.CharField(max_length = 250, default = "", verbose_name = "NOMBRE")
+    EsCargo = models.CharField(max_length = 20, default = "", verbose_name = "ES CARGO")
+
+    class Meta():
+        verbose_name="Cargos"
+        verbose_name_plural = "Empleos y Cargos"
+    def __str__(self):
+        return self.NomCargo
+
+class Logros (models.Model):
+    idLogros = models.IntegerField(verbose_name = "idLogros")
+    idTipoLogr = models.ForeignKey(TipoLogr, on_delete = models.CASCADE,verbose_name = "idTipoLogro")
+    idTipoUsua = models.IntegerField()
+    FechLogr = models.DateField(auto_now=False, verbose_name = "Fecha")
+    NombrLogr = models.CharField(max_length = 255, default = "", verbose_name = "Nombre")
+    DescLogr = models.TextField(verbose_name = "Descripcion")
+
+    class Meta():
+        verbose_name = "Logro"
+        verbose_name_plural = "Logros"
+    def __str__(self):
+        return self.NombrLogr
